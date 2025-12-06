daef2090817132dc68a10c0693b07021e39ae396
Kelangi Chinmai Sai Laxmi Priya <chinmai0605@gmail.com>
2025-12-06 19:37:52 +0530

    AdvanceProgramming Assignment Task1 - 3 Codes

--- AdvancePyProgrammers.py
+++ AdvancePyProgrammers.py
@@ -7,3 +7,18 @@ class Solution:
             total += num
             running_sum.append(total)
         return running_sum
+
+# 2. Shuffle the Array
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result = []
+        for i in range(n):
+            result.append(nums[i])
+            result.append(nums[i+n])
+        return result
+
+# 3. Kids With the Greatest Number of Candies
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+        return [candy + extraCandies >= max_candies for candy in candies]
