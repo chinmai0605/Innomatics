b609445bed6483bcc3abcc4165f6c57ad4ef6b32
Kelangi Chinmai Sai Laxmi Priya <chinmai0605@gmail.com>
2025-12-17 10:09:21 +0530

    Solved second question

--- ProgrammingTask-2_EDA.py
+++ ProgrammingTask-2_EDA.py
@@ -4,3 +4,17 @@ class Solution:
     def defangIPaddr(self,address: str)->str:
         return address.replace('.','[.]')
         
+# 2. Find Numbers with Even Number of Digits
+
+class Solution:
+    def findNumbers(self, nums: List[int])->int:
+        count = 0
+        for i in nums:
+            digits = 0
+            temp = i
+            while temp > 0:
+                temp //= 10
+                digits += 1
+            if digits % 2 == 0:
+                count += 1
+        return count
